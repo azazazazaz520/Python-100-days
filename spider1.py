3bc3776a889c332f273787c7c3a4ac4274388821
耀天 闫 <3219412675@qq.com>
2025-09-03 20:55:38 +0800

    9.3python request获取html源码

--- spider1.py
+++ spider1.py
@@ -1,3 +1,15 @@
 import requests as req
-response = req.get('https://www.taobao.com/')
-print(response)
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
+}
+
+try:
+    response = req.get('https://movie.douban.com/', headers=headers)
+    if response.status_code == 200:
+        print("Request successful")
+        #print(response.text)
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+except req.exceptions.RequestException as e:
+    print(f"Error occurred: {e}")
