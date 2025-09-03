a09e96d66f7cb42bbe118cc81a97054e3d747a3a
耀天 闫 <3219412675@qq.com>
2025-09-03 21:18:34 +0800

    9.3爬虫

--- spider1.py
+++ spider1.py
@@ -1,5 +1,5 @@
 import requests as req
-
+from bs4 import BeautifulSoup
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
 }
@@ -13,3 +13,9 @@ try:
         print(f"Request failed with status code: {response.status_code}")
 except req.exceptions.RequestException as e:
     print(f"Error occurred: {e}")
+
+
+soup = BeautifulSoup(response.text, 'html.parser')
+all_film_name = soup.find_all('li', class_='title')
+for film in all_film_name:
+    print(film.get_text(strip=True))
