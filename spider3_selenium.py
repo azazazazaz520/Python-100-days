0a1c308db54d8b8c2c78f478cddd66bb77da23fe
耀天 闫 <3219412675@qq.com>
2025-09-04 22:58:06 +0800

    9.4 spider3_selenium

---
+++ spider3_selenium.py
@@ -0,0 +1,13 @@
+from selenium import webdriver as wd     #用于操作浏览器
+from selenium.webdriver.chrome.service import Service 
+from selenium.webdriver.chrome.options import Options           #用于设置google浏览器的无头模式
+
+
+q1 = Options()
+#禁用沙盒模式
+q1.add_argument('--no-sandbox')
+#保持浏览器的打开状态
+q1.add_experimental_option(name='detach', value=True)
+
+#创建并启动浏览器
+a1 = wd.Chrome(service = Service('chromedriver.exe'), options=q1)
