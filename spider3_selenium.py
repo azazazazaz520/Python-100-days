aa038f07487e312aee47d883a53e7497f106e1cc
耀天 闫 <3219412675@qq.com>
2025-09-05 23:38:54 +0800

    9.5 selenium

--- spider3_selenium.py
+++ spider3_selenium.py
@@ -1,7 +1,7 @@
 from selenium import webdriver as wd     #用于操作浏览器
 from selenium.webdriver.chrome.service import Service 
 from selenium.webdriver.chrome.options import Options           #用于设置google浏览器的无头模式
-
+from time import sleep
 
 q1 = Options()
 #禁用沙盒模式
@@ -11,3 +11,6 @@ q1.add_experimental_option(name='detach', value=True)
 
 #创建并启动浏览器
 a1 = wd.Chrome(service = Service('chromedriver.exe'), options=q1)
+
+#打开一个网页
+a1.get('https://www.baidu.com')
